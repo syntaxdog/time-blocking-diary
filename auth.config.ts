a60705420866022a6669eae25a359f452ba12795
syntaxdog <kyie0913@naver.com>
2026-03-11 15:22:10 +0900

    feat: Deploy_Preparation

---
+++ auth.config.ts
@@ -0,0 +1,41 @@
+import Google from 'next-auth/providers/google';
+import GitHub from 'next-auth/providers/github';
+import Kakao from 'next-auth/providers/kakao';
+import type { NextAuthConfig } from 'next-auth';
+
+export default {
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+    Kakao({
+      clientId: process.env.AUTH_KAKAO_ID,
+      clientSecret: process.env.AUTH_KAKAO_SECRET,
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      if (session.user && token.id) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
+  },
+} satisfies NextAuthConfig;
